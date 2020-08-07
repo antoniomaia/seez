2f663050c313c31b9292b7ab89d24fd175850fc8
António Maia <antoniomaia@MacBook-Pro-de-Antonio.local>
2020-08-07 18:58:10 +0100

    update error message comp

--- components/error-message/index.js
+++ components/error-message/index.js
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
+import styles from './error.module.css';
 
 const ErrorMessage = ({ message }) => (
-  <div>
+  <article className={styles.error}>
     {message.map((error) => (
       <h4 key={error}>{error}</h4>
     ))}
-  </div>
+  </article>
 );
 
 ErrorMessage.propTypes = {
   message: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
+
 export default ErrorMessage;
