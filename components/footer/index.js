8edd18cefe16f1c8181fe3c9dff720201a25fd55
António Maia <antoniobcmaia@gmail.com>
2020-08-09 18:58:40 +0100

    update footer and search input styles

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 const footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.line} />
+      <span className={styles.line} />
       <p>
         <small>Developed by António Maia</small>
       </p>
